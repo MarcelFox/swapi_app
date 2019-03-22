5bd6b715ec052ef7fbd2044ac3cc669d568caa48
Marcel Fox <marcelfox@live.com>
2019-03-22 13:21:44 +0000

    Adjusting styles

--- app.js
+++ app.js
@@ -5,22 +5,12 @@ const router = express.Router();
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 app.use(express.static(__dirname+'/public'));
 app.use('/css', express.static(__dirname + '/public'));
 app.use('/js', express.static(__dirname + '/public'));
 
-//router.get('/about',function(req,res){
-//  res.sendFile(path.join(__dirname+'/about.html'));
-//});
-//
-//router.get('/sitemap',function(req,res){
-//  res.sendFile(path.join(__dirname+'/sitemap.html'));
-//});
-
-//add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
 
--- public/js/main.js
+++ public/js/main.js
@@ -16,11 +16,19 @@ const url = 'https://swapi.co/api/films/?format=json';
       let episode = document.createElement('li');
       let ul = document.createElement('ul');
 
+      ul.setAttribute("class", "list-group");
+
       title.innerHTML = `Title: ${chunk.title}`;
       release.innerHTML = `Release Date: ${chunk.release_date}`;
       episode.innerHTML = `Episode: ${chunk.episode_id}`;
         
-        document.body.appendChild(ul);
+      title.setAttribute("class", "list-group-item active");
+      release.setAttribute("class", "list-group-item");
+      episode.setAttribute("class", "list-group-item");
+
+      let column = document.getElementById("main_grid");
+
+        column.appendChild(ul);
         ul.appendChild(title);    
         ul.appendChild(release);    
         ul.appendChild(episode);
